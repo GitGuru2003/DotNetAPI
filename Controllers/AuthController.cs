c51f01720e55252b05d2463797781d43df39f76d
GitGuru2003 <email2munib@gmail.com>
2025-04-06 01:05:47 -0600

    Login Endpoint and GetPasswordHash method

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -35,15 +35,19 @@ namespace DotNetAPI.Controllers
             rng.GetNonZeroBytes(passwordSalt);
           }
 
-          string? passwordSaltString = _config.GetSection("AppSettings:PasswordKey").Value
-                                        + Convert.ToBase64String(passwordSalt);
-          byte[] passwordHash = KeyDerivation.Pbkdf2(
-            password: userForRegistration.Password,
-            salt: Encoding.ASCII.GetBytes(passwordSaltString),
-            prf: KeyDerivationPrf.HMACSHA256,
-            iterationCount: 100000,
-            numBytesRequested: 256 / 8
-          );
+
+          // string? passwordSaltString = _config.GetSection("AppSettings:PasswordKey").Value
+          //                               + Convert.ToBase64String(passwordSalt);
+          // byte[] passwordHash = KeyDerivation.Pbkdf2(
+          //   password: userForRegistration.Password,
+          //   salt: Encoding.ASCII.GetBytes(passwordSaltString),
+          //   prf: KeyDerivationPrf.HMACSHA256,
+          //   iterationCount: 100000,
+          //   numBytesRequested: 256 / 8
+          // );
+
+          byte[] passwordHash = GetPasswordHash(userForRegistration.Password, passwordSalt);
+
           string sqlAddAuth = @$" INSERT INTO TutorialAppSchema.Auth ([Email], [PasswordHash], [PasswordSalt])
                                    VALUES ('{userForRegistration.Email}', @PasswordHash, @PasswordSalt);";
 
@@ -73,7 +77,41 @@ namespace DotNetAPI.Controllers
     [HttpPost("Login")]
     public IActionResult Login(UserForLoginDto userForLogin)
     {
+
+      string sqlForHashAndSalt = @$"SELECT [PasswordHash], [PasswordSalt] FROM TutorialAppSchema.Auth
+                                 WHERE Email = '{userForLogin.Email}';";
+      UserForLoginConfirmationDto userForConfirmation = _dapper.LoadDataSingle<UserForLoginConfirmationDto>(sqlForHashAndSalt);
+
+      byte[] passwordHash = GetPasswordHash(userForLogin.Password, userForConfirmation.PasswordSalt);
+
+      for (int i = 0; i < passwordHash.Length; i++)
+      {
+        if (passwordHash[i] != userForConfirmation.PasswordHash[i])
+        {
+          return StatusCode(401, "Invalid Password");
+        }
+      }
       return Ok();
+
+      // if (passwordHash.SequenceEqual(userForConfirmation.PasswordHash))
+      // {
+      // }
+      // throw new Exception("Invalid password");
+    }
+
+    private byte[] GetPasswordHash(string password, byte[] passwordSalt)
+    {
+      string? passwordSaltString = _config.GetSection("AppSettings:PasswordKey").Value
+                                        + Convert.ToBase64String(passwordSalt);
+      byte[] passwordHash = KeyDerivation.Pbkdf2(
+        password: password,
+        salt: Encoding.ASCII.GetBytes(passwordSaltString),
+        prf: KeyDerivationPrf.HMACSHA256,
+        iterationCount: 100000,
+        numBytesRequested: 256 / 8
+      );
+
+      return passwordHash;
     }
 
 
